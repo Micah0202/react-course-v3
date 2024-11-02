58ba4e35d0c98ceaa845efd272fd9f23408c8aa1
Micah0202 <philipmicah0707@gmail.com>
2024-11-02 12:23:04 +0530

    finished strapi project

--- 04-fundamental-projects/13-strapi-submenus/starter/src/Navbar.jsx
+++ 04-fundamental-projects/13-strapi-submenus/starter/src/Navbar.jsx
@@ -4,16 +4,22 @@ import { useGlobalContext } from "./Context";
 import NavLinks from "./NavLinks";
 const Navbar = () => {
   //get the function  that will open up  the sidebar (for small screen)
-  const { openSidebar } = useGlobalContext();
+  const { openSidebar, setPageId } = useGlobalContext();
+  const handleSubmenu = (e) => {
+    // console.log(e.target);
+    if (!e.target.classList.contains("nav-link")) {
+      setPageId(null);
+    }
+  };
   return (
-    <nav>
+    <nav onMouseOver={handleSubmenu}>
       <div className="nav-center">
         <h3 className="logo">strapi</h3>
         <button className="toggle-btn" onClick={openSidebar}>
           <FaBars />
         </button>
         {/*nav links later */}
-        <NavLinks/>
+        <NavLinks />
       </div>
     </nav>
   );
--- 04-fundamental-projects/13-strapi-submenus/starter/src/Submenu.jsx
+++ 04-fundamental-projects/13-strapi-submenus/starter/src/Submenu.jsx
@@ -1,14 +1,31 @@
 import { useGlobalContext } from "./Context";
 import sublinks from "./data";
+import { useRef } from "react";
 
 //only for big screen layout
 const Submenu = () => {
-  const { pageId } = useGlobalContext(); //get the state variable from the global  context
+  const { pageId, setPageId } = useGlobalContext(); //get the state variable from the global  context
   const currentPage = sublinks.find((item) => item.pageId === pageId); //initially undefined but when we hover we  get the pageId dont show the submenu  if the currentPage is undefined
+
+  const submenuContainer = useRef(null);
+
+  const handleMouseLeave = (event) => {
+    const submenu = submenuContainer.current;
+    const { left, right, bottom } = submenu.getBoundingClientRect();
+    const { clientX, clientY } = event;
+
+    if (clientX < left - 1 || clientX > right - 1 || clientY > bottom - 1) {
+      setPageId(null);
+    }
+  };
 
   //use optional chaining as the initial  value in the global context for the state variable is null
   return (
-    <div className="submenu">
+    <div
+      className={currentPage ? "submenu show-submenu" : "submenu"}
+      onMouseLeave={handleMouseLeave}
+      ref={submenuContainer}
+    >
       <h5>{currentPage?.page}</h5>
       {/*render the sublinks  */}
       <div
